f6c86589c14a4a66c9bcf990354750ccf8f03dd6
Brian Ridolce <brianridolce@gmail.com>
2020-04-23 15:06:05 +0200

    initialisation du site

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,18 @@
+import Typed from 'typed.js';
+
+const text = document.querySelector('#banner-typed-text');
+
+const loadDynamicBannerText = () => {
+  if (text) {
+    new Typed('#banner-typed-text', {
+      strings: ["Fullstack Developer", "Ruby on Rails", "React.js"],
+      typeSpeed: 75,
+      loop: true,
+      showCursor: false
+    });
+  }
+}
+
+export {
+  loadDynamicBannerText
+};
