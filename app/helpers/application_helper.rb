9d7bc15bad58d6eb3ccc37f387a7f47f26d2d51a
Ridolce Brian <brianridolce@gmail.com>
2020-05-08 21:05:49 +0200

    hover sur logos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  # def title(title_content)
+  #   if title_content.present?
+  #     title_content << " | Brian Ridolce - Fullstack Développeur "
+  #   else
+  #     DEFAULT_META['title']
+  #   end
+
 end
